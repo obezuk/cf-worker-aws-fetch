7756d9431c5611e44d47515d4a5e23d690cc3d08
Tim Obezuk <tim@cloudflare.com>
2019-03-13 13:05:26 +1100

    SQS Example

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ const aws = new AwsClient({
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
-});a``
+});
 
 async function handleRequest(event) {
 
   var request = event.request;
 
   var url = new URL(event.request.url);
-
-  var bucketHostname = 'example.s3.ap-southeast-2.amazonaws.com';
-  url.hostname = bucketHostname;
+  var queue = 'https://sqs.ap-southeast-2.amazonaws.com/1234512345/worker-queue';
+  var sqs = new URL(queue);
+  sqs.searchParams.set('Action', 'SendMessage');
+  sqs.searchParams.set('Version', '2012-11-05');
+  sqs.searchParams.set('MessageBody', 'Hello World!');
 
   return await aws.fetch(url);
 
